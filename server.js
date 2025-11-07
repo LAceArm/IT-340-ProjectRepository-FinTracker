ef104252274f4d2e2ff639829362edf4a0897863
LArmGalvez <lacevedogalvez@gmail.com>
2025-11-07 10:48:46 -0500

    Added connection to the mongodb database

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express=require('express');
 
+const mongoose=require('mongoose');
+
 const app=express();
+
+const dbURL='mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.5.9'
+
 
 const port=3000;
 
@@ -11,4 +16,12 @@ app.get('/',(req,res)=>{
 
 app.listen(port, () => {
 console.log(`Server is running on port ${port}`);
+});
+
+mongoose.connect(dbURL,{
+    useNewUrlParser:true,
+    useUnifiedTopology:true
+}).then(()=>console.log('Connected to MongoDB'))
+.catch((error) => {
+    console.error('Error connecting to MongoDB:', error.message);
 });
