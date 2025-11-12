89c99c7ed4dc6a12894c13163fb7a469496180b8
LArmGalvez <lacevedogalvez@gmail.com>
2025-11-12 13:34:09 -0500

    Added a Mongoose schema to represent users

--- backend/server.js
+++ backend/server.js
@@ -7,11 +7,25 @@ app.use(cors());
 app.use(express.json());
 
 mongoose.connect('mongodb://localhost:27017/meanDB', { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('✅ MongoDB connected'))
-  .catch(err => console.error('❌ MongoDB connection error:', err));
+  .then(() => console.log(' MongoDB connected'))
+  .catch(err => console.error(' MongoDB connection error:', err));
 
+const user=new mongoose.Schema({
+  email: {
+    type:String,
+    required:true
+  },
+  password: {
+    type:String,
+    required:true
+  },
+  username:String,
+  budget: [],
+  expenses: []
+})
 app.get('/', (req, res) => {
   res.send('Backend is running');
 });
 
-app.listen(3000, '0.0.0.0', () => console.log('🚀 Server running on port 3000'));
+app.listen(3000, '0.0.0.0', () => console.log(' Server running on port 3000'));
+
