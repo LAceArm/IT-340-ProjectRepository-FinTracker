95f0c4b2733704986a9065ff8eb3d25463a088c9
LArmGalvez <lacevedogalvez@gmail.com>
2025-11-17 14:46:37 -0500

    Changed the schema to add a unique User ID

--- backend/server.js
+++ backend/server.js
@@ -11,6 +11,10 @@ mongoose.connect('mongodb://localhost:27017/meanDB', { useNewUrlParser: true, us
   .catch(err => console.error(' MongoDB connection error:', err));
 
 const user=new mongoose.Schema({
+  id: {
+    type:String,
+    required:true
+  },
   email: {
     type:String,
     required:true
