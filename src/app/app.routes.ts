53aff64ced25410cbedfc54a006d6f55c009e42d
Br7274 <bryanmuss7274@icloud.com>
2025-11-30 23:12:09 +0000

    Added registration page everything is working perfectly

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,9 @@
 import { Routes } from '@angular/router';
+
 import { LoginComponent } from './pages/login/login';
+import { RegisterComponent } from './pages/register/register';
 
 export const routes: Routes = [
-  { path: '', component: LoginComponent }
+  { path: '', component: LoginComponent },
+  { path: 'register', component: RegisterComponent }
 ];
